ec387e53b84790c9c2993a5c34169ef3af0ac046
Brett Patterson <bep5088@rit.edu>
2018-01-17 16:24:42 -0500

    worked on removeDuplicates function. need to finish it

--- Convert to jpg.py
+++ Convert to jpg.py
@@ -4,12 +4,12 @@ from PIL import Image
 
 def createFolder(user):
     """
-    Desc: This
+    Desc: This function looks at the desktop and creates a folder to store the hidden windows wallpapers
     :param user: The name of the user. It's writen so it can be run on any computer
     :return: Nothing
     """
-    if not os.path.exists("C:"+ os.path.sep+"Users" + os.path.sep + user+"\Desktop" + os.path.sep + "Windows Background Photos"):
-        os.makedirs("C:" + os.path.sep + "Users" + os.path.sep + user +"\Desktop" + os.path.sep + "Windows Background Photos")
+    if not os.path.exists("C:" + os.path.sep + "Users" + os.path.sep + user + "\Desktop" + os.path.sep + "Windows Background Photos"):
+        os.makedirs("C:" + os.path.sep + "Users" + os.path.sep + user + "\Desktop" + os.path.sep + "Windows Background Photos")
         print("Made Windows hidden files folder")
     else:
         print("The file existed already")
@@ -23,7 +23,7 @@ def copyFiles(src, dest):
     """
     src_files = os.listdir(src)
 
-    # this function is what copies the files to the dest
+    # this function is what copies the files to the destination
     for file_name in src_files:
         full_file_name = os.path.join(src, file_name)
         if (os.path.isfile(full_file_name)):
@@ -54,31 +54,66 @@ def removePictures( dest ):
     """
     lst = os.listdir(dest)
     remove = []
-    for num in range( len(lst) ):
+    for num in range(len(lst)):
         try:
             image = lst[num]
-            im = Image.open( image )
+            im = Image.open(image)
             Width = im.width
             if Width < 1200:
-                print( "small photo")
-                remove.append( dest+ os.path.sep + lst[num] )
+                print("small photo")
+                remove.append(dest + os.path.sep + lst[num])
         except IOError:
             print("not an image")
-            remove.append(dest+ os.path.sep + lst[num])
-        im.close()
-    for photo in range( len(remove)):
+            remove.append(dest + os.path.sep + lst[num])
+            im.close()
+    for photo in range(len(remove)):
         print("deleted")
         os.remove(remove[photo])
 
+
+def removeDuplicates(wallpaperFolder, dest):
+    hiddenPhotolst = os.listdir(dest)
+    wallpaperFolderLst = os.listdir(wallpaperFolder)
+    remove = []
+    for hiddenPhotoNum in range(len(hiddenPhotolst)):
+        for wallpaperPhotoNum in range(len(wallpaperFolderLst)):
+
+            try:
+                image = lst[num]
+                im = Image.open(image)
+                imgWidth = im.width
+                if imgWidth < 1200:
+                    print("small photo")
+                    remove.append(dest + os.path.sep + lst[num])
+            except IOError:
+                print("not an image")
+                remove.append(dest + os.path.sep + lst[num])
+                im.close()
+
+
+
+        for photo in range(len(remove)):
+            print("deleted")
+            os.remove(remove[photo])
+
+
+
+
+
 def main():
-    user = str( os.getlogin() ) #this gets the user name and checks to see if the dest folder exists already
-    src = os.path.abspath("C:"+os.path.sep+"Users"+ os.path.sep + user+"\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets")
-    dest = os.path.abspath("C:" + os.path.sep + "Users" + os.path.sep + user +"\Desktop" + os.path.sep + "Windows Background Photos")
+    user = str(os.getlogin()) #this gets the user name and checks to see if the dest folder exists already
+    src = os.path.abspath("C:" + os.path.sep + "Users" + os.path.sep + user+"\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets") # hidden file location
+    dest = os.path.abspath("C:" + os.path.sep + "Users" + os.path.sep + user + "\Desktop" + os.path.sep + "Windows Background Photos")
+
+    wallpaperFolder = os.path.abspath("C:" + os.path.sep + "Users" + os.path.sep + user + "\OneDrive" + os.path.sep + "wallpapers")
 
     createFolder(user)
     copyFiles(src, dest)
-    convertFilesToJPGs( dest )
-    removePictures( dest )
+    convertFilesToJPGs(dest)
+    removePictures(dest)
+
+    
+    removeDuplicates(wallpaperFolder, dest)
 
 
 
